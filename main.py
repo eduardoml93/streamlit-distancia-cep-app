c38becf5335821f91e8b8b597c7edb3f5d965282
Eduardo Moreni Lopes <94224260+eduardoml93@users.noreply.github.com>
2025-04-01 23:24:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -38,59 +38,6 @@ st.markdown(f"""
 </style>
 """, unsafe_allow_html=True)
 
-# Chave de API da OpenWeatherMap (substitua pela sua chave)
-API_KEY = "a5676ce9dbe81f9ddad2125c4dedb9b6"
-
-def obter_previsao_tempo(cidade):
-    endpoint = "http://api.openweathermap.org/data/2.5/weather"
-    params = {
-        "q": cidade,
-        "appid": API_KEY,
-        "units": "metric",  # Use "imperial" para Fahrenheit ou "metric" para Celsius
-        "lang": "pt_br"  
-    }
-
-    response = requests.get(endpoint, params=params)
-
-    if response.status_code == 200:
-        dados_clima = response.json()
-        return dados_clima
-    else:
-        st.error(f"Erro ao obter dados de previsão do tempo. Código de status: {response.status_code}")
-        return None
-
-def exibir_previsao_tempo(dados_clima):
-    if dados_clima:
-        st.write(f"**Cidade:** {dados_clima['name']}")
-        st.write(f"**País:** {dados_clima['sys']['country']}")
-        st.write(f"**Temperatura Atual:** {dados_clima['main']['temp']}°C")
-        st.write(f"**Tempo:** {dados_clima['weather'][0]['description'].capitalize()}")
-        return (dados_clima['coord']['lat'], dados_clima['coord']['lon'], dados_clima)
-    else:
-        st.warning("Insira uma cidade válida.")
-        return None
-
-def exibir_mapa(latitude, longitude, dados_clima):
-    st.write("**Mapa da Cidade**")
-    m = folium.Map(location=[latitude, longitude], zoom_start=10)
-    
-    # Criar o conteúdo do pop-up sem folium.Popup
-    pop_up_content = f"""
-    <b>Cidade:</b> {dados_clima['name']}<br>
-    <b>País:</b> {dados_clima['sys']['country']}<br>
-    <b>Temperatura Atual:</b> {dados_clima['main']['temp']}°C<br>
-    <b>Tempo:</b> {dados_clima['weather'][0]['description'].capitalize()}
-    """
-
-    # Adicionar o marcador com o conteúdo do pop-up
-    folium.Marker(
-        location=[latitude, longitude],
-        popup=folium.Popup(folium.Html(pop_up_content, script=True)),
-        icon=folium.Icon(color='blue')
-    ).add_to(m)
-
-    folium_static(m)
-
 # Function to fetch coordinates from CEP API
 
 def obter_coordenadas_cep(cep):
